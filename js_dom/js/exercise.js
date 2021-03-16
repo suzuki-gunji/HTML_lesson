52cd3fceff2276392a5e783c9c9e6c90c1a4584a
suzuki-gunji <gunji1102@gmail.com>
2021-03-16 21:14:07 +0900

    フォームに入力された値をDOMを用いて表示

---
+++ js_dom/js/exercise.js
@@ -0,0 +1,22 @@
+// 「ノードを追加」のレクチャーで学習したコードを改修、
+// ユーザーがテキストボックスに文字列を入力後
+// ユーザーがボタンを押すと
+// リストの最後の項目として文字列が追加されるプログラムを書いてみましょう。
+
+// 補足
+// テキストボックスの要素を取得するには、getElementByIDを利用できる。
+
+function append() {
+  // li要素を生成
+  var li = document.createElement('li');
+  // テキストノードを生成
+  var element = document.getElementById("text")
+  
+  var text = document.createTextNode(element.value);
+
+  // liタグの要素に、テキストノード textを追加
+  li.appendChild(text);
+  // idがlistsのulタグに、liを追加。具体的には<li>追加文字列</li>が、追加される。
+  var listsElement = document.getElementById("lists")
+  listsElement.appendChild(li)
+}
