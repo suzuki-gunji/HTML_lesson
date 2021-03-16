64556d0a22bc3684dbeba371be97b7a8b9840bad
suzuki-gunji <gunji1102@gmail.com>
2021-03-16 20:30:28 +0900

    クラスをキーに要素を取得

--- js_dom/js/elements_name.js
+++ js_dom/js/elements_name.js
@@ -2,7 +2,14 @@
 // ボタンをクリックしたら、
 // name属性が’result’の要素を取得して、その入力内容（テキスト）をコンソール表示する。
 
+// function showElements(){
+//   var elements = document.getElementsByName("result")
+//   console.log(elements[0].value)
+// }
+
 function showElements(){
-  var elements = document.getElementsByName("result")
-  console.log(elements[0].value)
+  var elements = document.getElementsByClassName("foo")
+  for (i = 0; i < elements.length; i++){
+    console.log(elements[i].innerText)
+  }
 }
