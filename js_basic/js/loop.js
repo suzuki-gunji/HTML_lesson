48c726d3e0cdf1c6991f3df76fe471f13a6d3160
suzuki-gunji <gunji1102@gmail.com>
2021-03-16 19:40:00 +0900

    for while do whileの繰り返し処理学習

---
+++ js_basic/js/loop.js
@@ -0,0 +1,19 @@
+// for(var i = 1; i<=3; i++){
+//   console.log(i);
+// }
+
+// var j =1;
+
+// while(j <= 3){
+//   console.log(j);
+//   j++;
+// }
+
+var sum = 0
+for(var i = 1; i<=100; i++){
+  if(i % 2 == 0){
+    sum += i;
+  }
+}
+
+console.log(sum);
