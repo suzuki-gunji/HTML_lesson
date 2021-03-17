f8e666cc2355bfa177d366baf0a78b7871b1a575
suzuki-gunji <gunji1102@gmail.com>
2021-03-17 10:56:46 +0900

    アクセサの学習(attr_accessor)

--- ruby/hello.rb
+++ ruby/hello.rb
@@ -1,12 +1,26 @@
 class User
+  attr_accessor :name
+  attr_reader :name
+  attr_writer :name
+
   def initialize(name)
     @name=name
   end
 
-  def hello
-    puts "#{@name}さん、hello"
-  end
+  # def name
+  #   @name
+  # end
+
+  # def name=(value)
+  #   @name=value
+  # end
+
+  # def hello
+  #   puts "#{@name}さん、hello"
+  # end
 end
  
-user = User.new("Mike")
-user.hello
+mike = User.new("Mike")
+puts mike.name
+mike.name="Emily"
+puts mike.name
