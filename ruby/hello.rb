fe3dab92189d1656c822e5e298ddd83bd1c2d1ff
suzuki-gunji <gunji1102@gmail.com>
2021-03-17 10:15:53 +0900

    メソッドの学習

--- ruby/hello.rb
+++ ruby/hello.rb
@@ -1,9 +1,29 @@
-age = 13
+# age = 13
 
-if age >= 12
-  print("大人")
-elsif age >= 6
-  print("中人")
-else
-  print("子ども")
-end  
+# if age >= 12
+#   print("大人")
+# elsif age >= 6
+#   print("中人")
+# else
+#   print("子ども")
+# end  
+
+# def hello
+#   puts "Hello World"
+# end
+
+# hello()
+
+# def fizz_buzz(n)
+#   if n % 15 == 0
+#     puts "FizzBuzz"
+#   elsif n % 5 == 0
+#     puts "Buzz"
+#   elsif n % 3 == 0
+#     puts "Fizz"
+#   else
+#     puts "他の数字です"
+#   end
+# end
+
+# fizz_buzz(5)
