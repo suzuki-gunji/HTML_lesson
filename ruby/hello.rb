1dfbc6e11ae45bd910f60e85da22d7447d59f6bf
suzuki-gunji <gunji1102@gmail.com>
2021-03-17 10:49:29 +0900

    クラスの学習

--- ruby/hello.rb
+++ ruby/hello.rb
@@ -1,57 +1,12 @@
-# age = 13
+class User
+  def initialize(name)
+    @name=name
+  end
 
-# if age >= 12
-#   print("大人")
-# elsif age >= 6
-#   print("中人")
-# else
-#   print("子ども")
-# end  
-
-# def hello
-#   puts "Hello World"
-# end
-
-# hello()
-
-# def fizz_buzz(n)
-#   if n % 15 == 0
-#     puts "FizzBuzz"
-#   elsif n % 5 == 0
-#     puts "Buzz"
-#   elsif n % 3 == 0
-#     puts "Fizz"
-#   else
-#     puts "他の数字です"
-#   end
-# end
-
-# fizz_buzz(5)
-
-# scores = {luke: 60, mike: 80, kyle: 100}
-
-# scores.each{|k, v|
-#   if v >= 80
-#     puts "すばらしい！"
-#   end
-# }
-
-# puts scores.keys
-
-# nums = [1,2,3,4,5]
-# # num.each do |num|
-# #   puts num
-# # end
-
-# # num.each {|num|
-# #   puts num
-# # }
-
-# for num in nums 
-#   puts num
-# end
-
-scores = {nakamura: 90, sato: 70, takahashi: 50}
-scores.each do |name, score|
-  puts "#{name}, #{score}"
+  def hello
+    puts "#{@name}さん、hello"
+  end
 end
+ 
+user = User.new("Mike")
+user.hello
